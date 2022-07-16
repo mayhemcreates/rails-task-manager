1d75864a6d2c41e80b09480727fcefd1483694b9
Harrie <mayhemcreates@gmail.com>
2022-07-16 13:04:24 +0100

    task list functionality complete

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,22 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # CREATE an invidiual task
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+
+  # READ show all tasks"
+  get "tasks", to: "tasks#index", as: :all_tasks
+
+  # READ an indvidual task
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  #UPDATE an individual task
+  get "tasks/:id/edit", to: "tasks#edit", as: :task_edit
+  patch "tasks/:id", to: "tasks#update"
+
+  #DELETE an individual task
+  delete "tasks/:id", to: "tasks#destroy"
+
 end
